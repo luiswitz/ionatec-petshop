f1859f8f298fab9d1f76fa7252d46a052f262007
Luis Henrique Witz <luiswitz@gmail.com>
2017-03-22 15:06:43 -0300

    Implemented sort on dogs table

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,7 @@
 class DogsController < ApplicationController
   def index
     @search = Dog.search(params[:q])
-    @dogs = @search.result.includes(:owner, :breed).page(params[:page]).per(10)
+    @search.sorts = ['id asc'] if @search.sorts.empty?
+    @dogs = @search.result.includes(:owner, :breed, :visits).page(params[:page]).per(10)
   end
 end
